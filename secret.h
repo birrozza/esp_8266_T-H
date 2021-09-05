92973ca82ffe4d5a3f8aab74a0e37d088f22c3ae
Angelo Birrozza <birrozza@libero.it>
2021-09-05 15:32:01 +0200

    Update secret.h
    change to default the secret id and secret api-key variable

--- secret.h
+++ secret.h
@@ -1,2 +1,2 @@
-#define SECRET_CH_ID 867219                                 // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "F32EPARZLNSE4W0K"                           // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 0000000                                 // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"                           // replace XYZ with your channel write API Key
