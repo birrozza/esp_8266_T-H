361fc3ed4b8f91a3c8eae29af41be7852f97e1ff
Angelo MIRABELLI <mirabelli_angelo@libero.it>
2023-11-11 08:43:24 +0100

    minor fix

--- Test_WiFiManager_thinkspeak_DHT22.ino
+++ Test_WiFiManager_thinkspeak_DHT22.ino
@@ -315,8 +315,8 @@ void loop() {
       String replay = "Stato: " + stato;
       replay += "\nCount: -"+String(count)+" min";
       replay += "\nLengt: "+String(console.length());
-      String ora = hour()<10 ? "0"+hour() : String(hour());
-      String minuto = minute()<10 ? "0"+minute() : String(minute());
+      String ora = hour()<10 ? "0"+String(hour()) : String(hour());
+      String minuto = minute()<10 ? "0"+String(minute()) : String(minute());
       replay += "\nSys time: "+ora+":"+minuto;
       myBot.sendMessage(msg, replay);
     } // stato
